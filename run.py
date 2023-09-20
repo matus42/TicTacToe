24a31c52a28681af766e1cccbe33266e603cbd24
matus42 <134312272+matus42@users.noreply.github.com>
2023-09-20 21:29:26 +0100

    Implement Player class with make_move method

--- run.py
+++ run.py
@@ -14,9 +14,29 @@ class TicTacToe:
         Prints the current state of the game board.
         """
         for i in range (0, 9, 3):
-            print(f"{self.board[i]} | {self.board[i + 1]} | {self.board[i + 2]}")    
+            print(f"{self.board[i]} | {self.board[i + 1]} | {self.board[i + 2]}")
+            
+class Player:
+    """Handles player actions."""
+    def make_move(self, board):
+        """
+        Takes input for the next move and updates the board
+        """
+        while True:
+            position = int(input("Your move! Choose a position (1-9): ")) - 1
+            if board[position] == ' ':
+                board[position] = 'X'
+                break
+            else:
+                print("Invalid move. Try again.")            
+                
 
 if __name__ == "__main__":
     game = TicTacToe()
-    print("Testing print_board method:")
+    player = Player()
+    print("Initial board:")
+    game.print_board()
+    
+    player.make_move(game.board)
+    print("Board after players move:")
     game.print_board()
