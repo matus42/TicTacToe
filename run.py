a9bc72666e12ad1619d0901468c8743e472bd40c
matus42 <134312272+matus42@users.noreply.github.com>
2023-09-20 21:03:51 +0100

    Implement board printing function

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+class TicTacToe:
+    """
+    Manages the game board and flow.
+    """
+    def __init__(self):
+        """
+        Initialize an empty game board.
+        """
+        self.board = [' ' for _ in range(9)]
+        
+    def print_board(self):
+        """
+        Prints the current state of the game board.
+        """
+        for i in range (0, 9, 3):
+            print(f"{self.board[i]} | {self.board[i + 1]} | {self.board[i + 2]}")    
 
 if __name__ == "__main__":
-    print("Tic-Tac-Toe Game")
+    game = TicTacToe()
+    print("Testing print_board method:")
+    game.print_board()
