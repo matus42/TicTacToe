2dd6a286cab0c52bfc794c2cde7d0b70227de362
matus42 <134312272+matus42@users.noreply.github.com>
2023-09-20 20:38:57 +0100

    Initialize project and add boilerplate code

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+if __name__ == "__main__":
+    print("Tic-Tac-Toe Game")
