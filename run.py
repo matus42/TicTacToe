a9ac2b9ad0357016d1512bed74d31dad7fbd36a5
matus42 <134312272+matus42@users.noreply.github.com>
2023-09-21 15:32:04 +0100

    Implement Computer class with make_move method

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
 class TicTacToe:
     """
     Manages the game board and flow.
@@ -20,7 +22,7 @@ class Player:
     """Handles player actions."""
     def make_move(self, board):
         """
-        Takes input for the next move and updates the board
+        Takes input for the next move and updates the board.
         """
         while True:
             position = int(input("Your move! Choose a position (1-9): ")) - 1
@@ -28,15 +30,37 @@ class Player:
                 board[position] = 'X'
                 break
             else:
-                print("Invalid move. Try again.")            
+                print("Invalid move. Try again.") 
+                
+                
+class Computer:
+    """Handles computer actions"""
+    
+    def make_move(self, board):
+        """
+        Makes a random move for the computer and updates the board.
+        """
+        position = random.choice([i for i, x in enumerate(board) if x == ' '])
+        board[position] = 'O'                           
                 
 
 if __name__ == "__main__":
     game = TicTacToe()
     player = Player()
+    computer = Computer()
     print("Initial board:")
     game.print_board()
     
+    #test player move
     player.make_move(game.board)
     print("Board after players move:")
     game.print_board()
+    
+    #test computers make_move method
+    print("Computer's turn:")
+    computer.make_move(game.board)
+    
+    print("Board after computer's move:")
+    game.print_board()
+    
+    
