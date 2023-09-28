30cf6ae6ff4dae44fa244bac219e3a24f3086179
matus42 <134312272+matus42@users.noreply.github.com>
2023-09-28 20:25:10 +0100

    Apply flake8 recommendations: remove white space, break lines over 80 characters.

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
 import os
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore, Style
 import time
 
 init(autoreset=True)
+
 
 def clearConsole():
     command = 'clear'
@@ -12,37 +13,39 @@ def clearConsole():
         command = 'cls'
     os.system(command)
 
-# clearConsole()
 
 class WelcomeScreen:
     @staticmethod
     def display():
-        print(Fore.CYAN + Style.BRIGHT + """
-                      
-  _______          ______               ______         
- /_  __(_)____    /_  __/___ ______    /_  __/___  ___ 
+        print(Fore.CYAN + Style.BRIGHT + r"""
+  _______          ______               ______
+ /_  __(_)____    /_  __/___ ______    /_  __/___  ___
   / / / / ___/_____/ / / __ `/ ___/_____/ / / __ \/ _ \\
  / / / / /__/_____/ / / /_/ / /__/_____/ / / /_/ /  __/
-/_/ /_/\___/     /_/  \__,_/\___/     /_/  \____/\___/ 
-                                                       
-      
+/_/ /_/\___/     /_/  \__,_/\___/     /_/  \____/\___/
         """)
         print(Fore.YELLOW + "===================================")
         print(Fore.YELLOW + "     Welcome to Tic-Tac-Toe!")
         print(Fore.YELLOW + "===================================")
         print("Here are the rules:")
-        print("1. The board has positions 1-9 starting from top-left and going row-wise.")
+        print("1. The board has positions 1-9 starting from top-left"
+              " and going row-wise.")
         print("2. You are 'X' and the computer is 'O'.")
-        print("3. To win, get three of your marks in a row, column, or diagonal.")
-        print("4. Input your move as a number between 1 and 9 to place your mark.")
+        print("3. To win, get three of your marks in a row, column,"
+              " or diagonal.")
+        print("4. Input your move as a number between"
+              " 1 and 9 to place your mark.")
         print("5. If the board fills up without a winner, it's a draw.")
         print("===================================")
-        
-        difficulty = input("Choose difficulty level - Easy (e), Medium (m), Hard (h): ").lower()
+
+        difficulty = input("Choose difficulty level - Easy (e), Medium (m),"
+                           " Hard (h): ").lower()
         while difficulty not in ['e', 'm', 'h']:
             print("Invalid option. Please choose again.")
-            difficulty = input("Choose difficulty level - Easy (e), Medium (m), Hard (h): ").lower()
-        return difficulty    
+            difficulty = input("Choose difficulty level - Easy (e),"
+                               " Medium (m), Hard (h): ").lower()
+        return difficulty
+
 
 class TicTacToe:
     """
@@ -56,21 +59,21 @@ class TicTacToe:
         self.player_score = 0
         self.computer_score = 0
         self.difficulty = difficulty
-        
+
     def display_score(self):
         """Displays the current score and difficulty."""
-        
+
         difficulty_mapping = {'e': 'Easy', 'm': 'Medium', 'h': 'Hard'}
         difficulty_name = difficulty_mapping.get(self.difficulty, "Unknown")
-        
+
         print(f"Difficulty Level: {difficulty_name}")
-        print(f"Current Score: Player {self.player_score} - Computer {self.computer_score}")
-        print("===================================")    
-        
-    
+        print(f"Current Score: Player {self.player_score} -"
+              " Computer {self.computer_score}")
+        print("===================================")
+
     def print_board(self):
         """Prints the game board."""
-        print(Fore.CYAN + Style.BRIGHT + "        Tic-Tac-Toe  ") 
+        print(Fore.CYAN + Style.BRIGHT + "        Tic-Tac-Toe  ")
         print(Fore.BLUE + "---------------------------")
         for i in range(0, 9, 3):
             row = []
@@ -81,12 +84,12 @@ class TicTacToe:
                     row.append(Fore.RED + self.board[i+j] + Style.RESET_ALL)
                 else:
                     row.append(self.board[i+j])
-            print(f" {i+1} | {i+2} | {i+3}       {row[0]} | {row[1]} | {row[2]} ")
+            print(f" {i+1} | {i+2} | {i+3} "
+                  f"      {row[0]} | {row[1]} | {row[2]} ")
             if i < 6:
                 print("---+---+---     ---+---+---")
         print(Fore.BLUE + "---------------------------")
-        
-            
+
     def check_winner(self, char):
         """
         Check for a winning condition for 'char' in game.
@@ -102,8 +105,7 @@ class TicTacToe:
         if self.board[2] == self.board[4] == self.board[6] == char:
             return True
         return False
-    
-    
+
     def check_draw(self):
         """Check for a draw"""
         if ' ' not in self.board:
@@ -111,7 +113,7 @@ class TicTacToe:
             print("It's a draw!")
             return True
         return False
-    
+
     def make_and_check_move(self, mover, symbol, win_message):
         """Make a move and check if it resulted in a win"""
         mover.make_move(self.board)
@@ -120,35 +122,33 @@ class TicTacToe:
             print(win_message)
             return True
         return False
-    
+
     def update_score(self, winner):
         """Update the score based on the winner."""
         if winner == 'Player':
             self.player_score += 1
         elif winner == 'Computer':
             self.computer_score += 1
-                
-    
-    
+
     def play_game(self):
         """
         Main game loop that brings all the functionalities together.
         """
-        
+
         player = Player()
         computer = Computer(difficulty, self.check_winner)
         game_count = 0
-        
+
         while True:
             self.board = [' ' for _ in range(9)]
-        
+
             while True:
                 clearConsole()
                 self.display_score()
                 self.print_board()
-                
+
                 if self.make_and_check_move(player, 'X', "Player wins!\n"):
-                    clearConsole()  # Clear console before showing the winning board and message
+                    clearConsole()
                     self.update_score('Player')
                     self.display_score()
                     self.print_board()
@@ -156,7 +156,7 @@ class TicTacToe:
                     time.sleep(2)
                     clearConsole()
                     break
-                
+
                 if self.check_draw():
                     clearConsole()
                     self.display_score()
@@ -165,16 +165,16 @@ class TicTacToe:
                     time.sleep(2)
                     clearConsole()
                     break
-                    
+
                 # Computers move
                 clearConsole()
                 self.display_score()
                 self.print_board()
                 print("Computer is thinking...")
                 time.sleep(1.3)
-            
+
                 if self.make_and_check_move(computer, 'O', "Computer wins"):
-                    clearConsole()  # Clear console before showing the winning board and message
+                    clearConsole()
                     self.update_score('Computer')
                     self.display_score()
                     self.print_board()
@@ -182,7 +182,7 @@ class TicTacToe:
                     time.sleep(2)
                     clearConsole()
                     break
-                
+
                 if self.check_draw():
                     clearConsole()
                     self.display_score()
@@ -191,38 +191,42 @@ class TicTacToe:
                     time.sleep(2)
                     clearConsole()
                     break
-            
+
             game_count += 1
             if game_count == 2:
                 clearConsole()
                 self.display_score()
                 self.print_board()
-                
+
                 print("You've played 2 games!")
                 if self.difficulty == 'h':
                     while True:
-                        choice = input("Do you want to keep playing? (y/n):").lower()
+                        choice = input("Do you want to keep playing?"
+                                       " (y/n):").lower()
                         if choice in ['y', 'n']:
                             break
                         print("Invalid input. Please choose 'y' or 'n'.")
                     if choice == 'n':
-                        return # exit the game                  
+                        return  # exit the game
                 else:
                     while True:
-                        choice = input("Do you want to increase difficulty or exit? (i/e).")
+                        choice = input("Do you want to increase"
+                                       " difficulty or exit? (i/e).")
                         if choice in ['i', 'e']:
                             break
-                        print("Invalid input. Please choose 'i'(increase) or 'e'(exit).")
+                        print("Invalid input. Please choose 'i'(increase)"
+                              " or 'e'(exit).")
                     if choice == 'e':
-                        return # exit the game
+                        return  # exit the game
                     elif choice == 'i':
                         if self.difficulty == 'e':
                             self.difficulty = 'm'
                         elif self.difficulty == 'm':
-                            self.difficulty = 'h' 
-                        computer = Computer(self.difficulty, self.check_winner)            
-                game_count = 0                      
-            
+                            self.difficulty = 'h'
+                        computer = Computer(self.difficulty, self.check_winner)
+                game_count = 0
+
+
 class Player:
     """Handles player actions."""
     def make_move(self, board):
@@ -231,7 +235,9 @@ class Player:
         """
         while True:
             try:
-                position = int(input("Your move! Choose a position (1-9): ")) - 1
+                position = int(input(
+                    "Your move! Choose a position (1-9): "
+                    )) - 1
                 if 0 <= position < 9:
                     if board[position] == ' ':
                         board[position] = 'X'
@@ -239,24 +245,25 @@ class Player:
                     else:
                         print("Invalid move. Try again.")
                 else:
-                    print("Invalid position. Choose a number between 1 and 9.")         
+                    print("Invalid position. Choose a number between 1 and 9.")
             except ValueError:
-                print("Please enter a number between 1 and 9.")    
-                
+                print("Please enter a number between 1 and 9.")
+
+
 class Computer:
     def __init__(self, difficulty, check_winner_func):
         self.difficulty = difficulty
         self.check_winner_func = check_winner_func
-        
+
     def find_winning_move(self, board, symbol):
         for i in range(9):
             if board[i] == ' ':
                 board[i] = symbol
                 if self.check_winner_func(symbol):
                     return True
-                board[i] = ' '    
-        return False 
-    
+                board[i] = ' '
+        return False
+
     def find_blocking_move(self, board):
         for i in range(9):
             if board[i] == ' ':
@@ -265,42 +272,34 @@ class Computer:
                     board[i] = 'O'
                     return True
                 board[i] = ' '
-        return False              
-        
+        return False
+
     def make_move(self, board):
         if self.difficulty == 'h':
             if self.find_winning_move(board, 'O'):
                 return
             if self.find_blocking_move(board):
                 return
-            
+
         elif self.difficulty == 'm':
             if self.find_winning_move(board, 'O'):
-                return    
-        
+                return
+
         available_positions = [i for i, x in enumerate(board) if x == ' ']
         if available_positions:
             position = random.choice(available_positions)
             board[position] = 'O'
-                
 
-# if __name__ == "__main__":
-#     difficulty = WelcomeScreen.display()
-#     clearConsole()
-#     game = TicTacToe(difficulty)
-#     game.play_game()
-    
+
 if __name__ == "__main__":
     while True:
+        clearConsole()
         difficulty = WelcomeScreen.display()
         clearConsole()
         game = TicTacToe(difficulty)
-        game.play_game()      
+        game.play_game()
 
         print("\nThank you for playing Tic-Tac-Toe!")
         choice = input("Would you like to play again? (y/n): ").lower()
         if choice == 'n':
             break
-     
-    
-    
